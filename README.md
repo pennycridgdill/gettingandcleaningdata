63db1cda4136fa4d962c09cc5689de3737c7323f
pennycridgdill <pennyridgdill@gmail.com>
2015-05-19 23:06:12 -0400

    This is the second commit, including the complete run_analysis script, README file and tidy data file

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# gettingandcleaningdata
-Repository for materials from the Getting and Cleaning Data course for Coursera
+# GettingAndCleaningData
+##Course Project Repository
+This is the README file for my repository for the Course Project Getting and Cleaning Data
+
+##Script Breakdown
+My run_analysis.r script works using the following basic steps:
+
+1. Read in the feature names (from features.txt), so that I can set those to the column names for the X_data
+
+2. Read in the data from the X_test data, assigning the column names using the features vector
+
+3. Read in the y_test data and the activity_labels table, and then assigng the activity integer values to their associated strings
+
+4. Read in the subject data
+
+5. Do a [grep search](https://stat.ethz.ch/R-manual/R-devel/library/base/html/grep.html) in the feature names for "mean" and "std", combine and sort the resulting vectors
+
+6. Bind the X, y and subject dataframes, and use the sorted vector from the previous step to subset the data to only include the desired measurements
+
+7. Bind a DataType column to the front of the frame, indicated that this data was "Test" data
+
+8. Repeat the Steps above for the training data, omitting redundant steps, such as reading in the feature names and activity labels
+
+9. Row bind the test and training tables
+
+10. Use the aggregate function to aggregate the measurement columns by Subject and Activity (*please* read the [documentation for aggregate](https://stat.ethz.ch/R-manual/R-patched/library/stats/html/aggregate.html) if you do not know how this works with formulae)
+
+11. Arrange the table that resulted from the step above, by subject, and drop the DataType column (since that wasn't part of the requested data)
+
+12. Write the resulting data/table to a text file
+
+
+##CodeBook
+Note: The CodeBook is included in a separate file within this repository
+
+
+
+
+
+
+
--- run_analysis.r
+++ run_analysis.r
@@ -1,4 +1,4 @@
-#This is the code for the course project for Getting and Cleaning Data
+#This is the code for the Course Project for Getting and Cleaning Data
 #For Coursera
 
 #input the data manipulation packages
@@ -12,21 +12,34 @@ featurenames <- read.table("data//UCI HAR Dataset/features.txt")
 xtestdata <- read.table("data//UCI HAR Dataset/test//X_test.txt",
                         col.names=featurenames$V2)
 
-#read in the activity type data
+#read in the activity type data, and the activity names
+#then set the activity type to the string for the activity it
+#represents
 ytestdata <- read.table("data//UCI HAR Dataset//test//y_test.txt",
                         col.names="Activity")
+activitynames <- read.table("data//UCI HAR Dataset/activity_labels.txt",
+                            stringsAsFactors=FALSE)
+ytestdata$Activity <- activitynames[ytestdata$Activity,]$V2
+
+
 
 #read in the subject data
 subdata <- read.table("data//UCI HAR Dataset//test//subject_test.txt",
                       col.names="Subject")
 
 
-
+#create vector of featurenames containing "mean" or "std"
+#shift by two for the offset from the first two columns
+myfeaturenames <- sort(c(grep("mean",featurenames$V2),grep("std",featurenames$V2)))
+myfeaturenames <- myfeaturenames+2
 
 
 #bind the testing data, column-wise, into a single data frame
+#then convert to tbl_df object so we can use dplyr/tidyr
 testframe <- cbind(subdata,ytestdata,xtestdata)
-
+testtable <- select(tbl_df(testframe),c(1,2,myfeaturenames))
+#bind a column to the front indicating that this was data from the test set
+testtable <- cbind(DataType=rep("Test",length(ytestdata)),testtable)
 
 
 #repeat the steps above, but with the training data
@@ -37,8 +50,29 @@ ytraindata <- read.table("data//UCI HAR Dataset/train//y_train.txt",
 subtraindata <- read.table("data//UCI HAR Dataset/train//subject_train.txt",
                            col.names="Subject")
 
-trainingframe <- cbind(subtraindata,ytraindata,xtraindata)
+ytraindata$Activity <- activitynames[ytraindata$Activity,]$V2
 
+trainingframe <- cbind(subtraindata,ytraindata,xtraindata)
+trainingtable <- select(tbl_df(trainingframe),c(1,2,myfeaturenames))
+trainingtable <- cbind(DataType=rep("Training",length(ytraindata)),trainingtable)
+
+
+#row bind the two data tables for the tidy data set for steps 1-4 in the project
+#note, this is the first tidy data set produced, but we were not
+#to actually export this data externally
+meanstddatatable <- rbind(testtable,trainingtable)
+
+#now, compute the means (averages) for each measurement, by subject and activity
+#for the final part of the project
+#please see the documentation for the aggregate function if you don't understand
+#how the formula works in the first argument
+aggregateddata <- aggregate(. ~ Subject+Activity,meanstddatatable,mean)
+
+#drop the DataType column, and arrange the aggregated data by Subject
+mytidytidydata <- arrange(select(letstrythis,-DataType),Subject)
+
+#write the tidy data to a text file
+write.table(mytidytidydata,"mytidytidydata.txt",row.names=FALSE)
 
 
 
